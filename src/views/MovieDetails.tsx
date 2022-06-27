efbe00aa1186efd5ef1faa4e5fbc5b01778973b8
sourabhdadapure <sourabhdadapure@gmail.com>
2022-06-26 21:37:33 -0400

    initializes movie details view

---
+++ src/views/MovieDetails.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+import { View, Text } from "react-native";
+
+export default class MovieDetails extends React.Component {
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <Text>MovieDetails</Text>
+      </View>
+    );
+  }
+}

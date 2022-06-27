d174bfa87f415c09506160e1bafe2b13a165243d
sourabhdadapure <sourabhdadapure@gmail.com>
2022-06-26 22:29:29 -0400

    Adds redux container on top of the app

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 /**
  * @format
  */
+import * as React from "react";
+import { AppRegistry } from "react-native";
+import { name as appName } from "./app.json";
+import configureStore from "./src/config/configStore";
+const store = configureStore();
+import { Provider } from "react-redux";
+import ViewStack from "./src/ViewStack";
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+const Root = () => (
+  <Provider store={store}>
+    <ViewStack />
+  </Provider>
+);
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => Root);

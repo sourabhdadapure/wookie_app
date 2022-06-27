6a1bf74fa867c975b3dd9ba9e23d5d39c1de7711
sourabhdadapure <sourabhdadapure@gmail.com>
2022-06-26 22:24:44 -0400

    adds getMovies request

--- src/modules/Movies/actions.ts
+++ src/modules/Movies/actions.ts
@@ -1,13 +1,30 @@
-import {Dispatch} from 'redux';
-import types from './types';
+import { Axios } from "axios";
+import { Dispatch } from "redux";
+import { MovieModel } from "./reducers";
+import types from "./types";
 
 export const getMovies = () => {
-  return (dispatch: Dispatch) => {
+  return async (dispatch: Dispatch) => {
     try {
-      dispatch({type: types.GET_MOVIES_LOADING});
-      dispatch({type: types.GET_MOVIES_SUCCESS, payload: []});
+      dispatch({ type: types.GET_MOVIES_LOADING });
+
+      const data: Partial<MovieModel> = await fetch(
+        "https://wookie.codesubmit.io/movies",
+        {
+          method: "GET",
+          headers: {
+            Accept: "application/json",
+            "Content-Type": "application/json",
+            Authorization: "Bearer Wookie2019",
+          },
+        }
+      );
+      console.warn("Data", data);
+      const payload = data.movies;
+
+      dispatch({ type: types.GET_MOVIES_SUCCESS, payload });
     } catch (error) {
-      dispatch({type: types.GET_MOVIES_ERROR, error});
+      dispatch({ type: types.GET_MOVIES_ERROR, error });
     }
   };
 };

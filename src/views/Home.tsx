42c1d564bd19d7b92eeef9c24d253c6f2273b506
sourabhdadapure <sourabhdadapure@gmail.com>
2022-06-26 22:41:31 -0400

    connects HomeScreen to redux

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,7 +1,14 @@
 import * as React from "react";
 import { View, Text } from "react-native";
+import { getMovies, MovieModel } from "../modules/Movies";
+import { connect } from "react-redux";
+interface HomeProperties {
+  getMovies(): void;
+  Movies: MovieModel;
+}
 
-export default class Home extends React.Component {
+@(connect((state: MovieModel) => state, { getMovies }) as any)
+export default class Home extends React.Component<HomeProperties> {
   render() {
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>

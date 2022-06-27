2a5fabd2f3818b7a1d4123dfbc60ae24a16b39ea
sourabhdadapure <sourabhdadapure@gmail.com>
2022-06-26 21:39:19 -0400

    initializes view stack

---
+++ src/ViewStack.tsx
@@ -0,0 +1,20 @@
+import * as React from "react";
+import { View } from "react-native";
+import { Home, MovieDetails } from "./views";
+import { Router, Route, Switch } from "react-router-native";
+import { createMemoryHistory } from "history";
+
+export const history = createMemoryHistory();
+
+export default class ViewStack extends React.Component {
+  render() {
+    return (
+      <Router history={history}>
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/movie/:id" component={MovieDetails} />
+        </Switch>
+      </Router>
+    );
+  }
+}

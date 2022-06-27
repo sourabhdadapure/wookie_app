a089a578e53242e0112e3399ae7dc36e534e3a6e
sourabhdadapure <sourabhdadapure@gmail.com>
2022-06-26 23:14:19 -0400

    removes console warn

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,18 +1,28 @@
 import * as React from "react";
 import { View, Text } from "react-native";
-import { getMovies, MovieModel } from "../modules/Movies";
+import { getMovies, MovieModel, MoviesModel } from "../modules/Movies";
 import { connect } from "react-redux";
+import MovieList from "../components/MovieList";
 interface HomeProperties {
   getMovies(): void;
-  Movies: MovieModel;
+  Movies: MoviesModel;
 }
 
 @(connect((state: MovieModel) => state, { getMovies }) as any)
 export default class Home extends React.Component<HomeProperties> {
+  componentDidMount() {
+    const { getMovies, Movies } = this.props;
+    getMovies();
+  }
   render() {
+    const { Movies } = this.props;
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <Text>Home</Text>
+        <MovieList
+          title="Movies"
+          scrollViewType="horizontal"
+          movies={Movies.movies}
+        />
       </View>
     );
   }
